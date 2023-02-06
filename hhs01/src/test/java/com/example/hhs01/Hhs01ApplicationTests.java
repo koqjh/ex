462ecf9646cf0aa18e6ca85b53aa047779d23b7a
hhs <koqjh@naver.com>
2023-02-06 16:28:03 +0900

    1

---
+++ hhs01/src/test/java/com/example/hhs01/Hhs01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hhs01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hhs01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
